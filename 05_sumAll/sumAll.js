1ac191df77f3ff3a1f4e92a8fe98e4d76e7153cf
JDan1092 <95508276+JDan1092@users.noreply.github.com>
2023-11-06 17:42:22 -0600

    Fifth exercise completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    let error = "ERROR";
+    let biggerValue = Math.max(num1, num2);
+    let smallerValue = Math.min(num1, num2);
+    if (((typeof num1 === "string")) || (typeof num2 === "string")) {
+        return error;
+    } else if (((typeof num1 === "object")) || (typeof num2 === "object")) {
+        return error;
+    } else if ((biggerValue >= 0) && (smallerValue >= 0)) {
+        for (let i = smallerValue; i <= biggerValue; i++) {
+            sum = sum +i;
+        }
+    } else return error;
+    return sum;
+    
 };
 
 // Do not edit below this line
