5cb8fcda886ef00f0c36dbe550a073ec728f989a
JDan1092 <95508276+JDan1092@users.noreply.github.com>
2023-11-07 17:49:27 -0600

    Sixth exercise completed

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,20 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    let dividedBy4 = year % 4;
+    let dividedBy100 = year % 100;
+    let dividedBy400 = year % 400;
 
+    if (dividedBy4 !== 0) {
+        return  false;
+    }
+    if (dividedBy4 === 0 && dividedBy100 !== 0) {
+        return true;
+    }
+    if (dividedBy4 === 0 && dividedBy100 === 0 && dividedBy400 === 0) {
+        return true;
+    } 
+    if (dividedBy4 === 0 && dividedBy100 === 0 && dividedBy400 !== 0) {
+        return false;
+    }
 };
 
 // Do not edit below this line
