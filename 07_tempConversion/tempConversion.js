0366d7a19ef4f74f9bbc4c34b3bdc2d2938b31ad
JDan1092 <95508276+JDan1092@users.noreply.github.com>
2023-11-09 17:23:23 -0600

    Seventh exercise completed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let fToCel = (fahrenheit - 32) * (5 / 9);
+  let cRounded = Math.round(fToCel * 10) / 10;
+  return cRounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let cToFahr = (celsius * (9 / 5) + 32);
+  let fRounded = Math.round(cToFahr * 10) / 10;
+  return fRounded;
 };
 
 // Do not edit below this line
