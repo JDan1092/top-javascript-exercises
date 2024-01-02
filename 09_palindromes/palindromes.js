5300b4e0988fb929540c953f0693f63a88153d11
JDan1092 <95508276+JDan1092@users.noreply.github.com>
2024-01-02 16:31:53 -0600

    Palindromes exercises completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    // let re = /[\W_]/g; ///[^A-Za-z0-9]/g;
+    // let lowRegStr = str.toLowerCase().replace(re, '');
+    // let reverseStr = lowRegStr.split('').reverse().join(''); 
+    // return reverseStr === lowRegStr;
 
+    let re = /[\W_]/g; ///[^A-Za-z0-9]/g;
+    str = str.toLowerCase().replace(re, '');
+    let len = str.length;
+    for (let i = 0; i < len/2; i++) {
+        if (str[i] !== str[len - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
