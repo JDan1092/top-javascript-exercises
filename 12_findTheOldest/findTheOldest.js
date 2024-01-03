f5e7bfa7f69492ce3f4b978c028c8513caa73058
JDan1092 <95508276+JDan1092@users.noreply.github.com>
2024-01-03 17:07:24 -0600

    The oldest exercises completed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,32 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    // let total =0; Used people in the "function(people)"
+    // let oldestPerson = {};
+    // people.reduce((prev, curr) => {
+    //     if ((curr.yearOfDeath - curr.yearOfBirth) > total) {
+    //         total = (curr.yearOfDeath - curr.yearOfBirth);
+    //         oldestPerson = curr;
+    //     }
+    //     return prev;
+    // })
+    // return oldestPerson; 
+    //The code above only worked for the first 2 exercises
 
+
+    return array.reduce((oldest, current) => {
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const currentAge = getAge(
+            current.yearOfBirth,
+            current.yearOfDeath
+        );
+        return oldestAge < currentAge ? current : oldest;
+    });
 };
 
+const getAge = function (birth, death) {
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+};
 // Do not edit below this line
 module.exports = findTheOldest;
